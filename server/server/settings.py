6e7b5172b5d3db7bf03193479b8242b726df2751
Oto <otodrahonovsky@yahoo.com>
2019-01-26 00:18:35 +0800

    Begun move to REST

--- server/server/settings.py
+++ server/server/settings.py
@@ -42,10 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'books'
+    'books',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,3 +132,5 @@ REST_FRAMEWORK = {
    'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
   ]
 }
+
+CORS_ORIGIN_ALLOW_ALL = DEBUG
